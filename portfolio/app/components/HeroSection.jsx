8c7ad9d22a182a1183c827f315ddd707e9223f1f
Chad Manuel <chadc_manuel@yahoo.com>
2023-10-09 22:36:18 -0700

    fixed parts of the hero page

--- portfolio/app/components/HeroSection.jsx
+++ portfolio/app/components/HeroSection.jsx
@@ -1,23 +1,48 @@
+"use client"
 import React from 'react';
 import Image from 'next/image';
+import { TypeAnimation } from 'react-type-animation';
+
 
 const HeroSection = () => {
   return (
     <section>
-        <div className='grid grid-cols-1 ;lg:grid-cols-12'>
-            <div className='col-span-7 place-self-center'>
-                <h1 className='text-white mb-4 text-4xl lg:text-6xl font-extrabold'>
-                    Hello, I'm Chad
+        <div className='grid grid-cols-1 sm:grid-cols-12'>
+            <div className='col-span-7 place-self-center text-center sm:text-left'>
+                <h1 className='text-white mb-4 text-4xl sm:text-4xl lg:text-6xl font-extrabold'>
+                    <span className='text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600'>
+                        Hello, I&apos;m{" "}
+                    </span>
+                    <br></br>
+                    <TypeAnimation
+                        sequence={[
+                            'Chad',
+                            3000,
+                            'A Front End Developer',
+                            1000,
+                            'A Back End Developer',
+                            1000,
+                            'A Full Stack Engineer',
+                            1000,
+                            'A Software Engineer',
+                            1000
+                        ]}
+                        wrapper='span'
+                        speed={50}
+                        repeat={Infinity}
+                    />
                 </h1>
-                <p className='text-[#ADB7BE] text-lg mb-6 lg:text-xl'>
+                <p className='text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl'>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
                 Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
                 Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
                 Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                 </p>
                 <div>
-                    <button className='px-6 py-3 rounded-full mr-4 bg-white hover:bg-slate-200 text-black'>Hire Me</button>
-                    <button className='px-6 py-3 rounded-full mr-4 bg-transparent hover:bg-slate-800 text-white border border-white mt-3'>Download Resume</button>
+                    <button className='px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white'>Hire Me</button>
+                    <button className='px-1 py-1 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white mt-3'>
+                        <span className='block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2'>Download Resume</span>
+                    </button>
                 </div>
             </div>
             <div className='col-span-5 place-self-center mt-4 lg:mt-0'>
