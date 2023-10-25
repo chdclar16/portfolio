d98d3d9ad3fa9f45c727160c23e74f2d8451f215
Chad Manuel <chadc_manuel@yahoo.com>
2023-10-25 15:36:19 -0700

    removed padding bottom from nav bar

--- app/components/NavBar.jsx
+++ app/components/NavBar.jsx
@@ -26,7 +26,7 @@ const NavBar = () => {
     
     return (
     <nav className='fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-100'>
-        <div className='flex flex-wrap items-center justify-between mx-auto px-4 py-2'>
+        <div className='flex flex-wrap items-center justify-between mx-auto pt-4 py-2 pb-0'>
             <Link 
                 to='hero'
                 smooth={true}
