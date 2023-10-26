910f0c0e30df5b3d2f8eea9438a4017b59d83a26
Chad Manuel <chadc_manuel@yahoo.com>
2023-10-25 17:36:24 -0700

    changed nav opacity

--- app/components/NavBar.jsx
+++ app/components/NavBar.jsx
@@ -25,7 +25,7 @@ const NavBar = () => {
     const [navBarOpen, setNavBarOpen] = useState(false)
     
     return (
-    <nav className='fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-100'>
+    <nav className='fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-50'>
         <div className='flex flex-wrap items-center justify-between mx-auto pt-4 py-2 pb-0'>
             <Link 
                 to='hero'
