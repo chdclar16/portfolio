23553e06e2eb7235313420ab757fe147fb154e27
Chad Manuel <chadc_manuel@yahoo.com>
2024-09-02 02:03:48 -0700

    wip: refactoring skills to use arrays of skills instead
    i refacotered the component to be easier to maintain since there is less
    repetition with the classnames

--- app/components/AboutSection.jsx
+++ app/components/AboutSection.jsx
@@ -1,248 +1,331 @@
 "use client";
-import React, { useTransition, useState } from 'react';
-import Image from 'next/image';
-import TabButton from './TabButton';
-import { Element } from 'react-scroll';
-import { motion } from 'framer-motion';
-import { FaReact } from 'react-icons/fa';
-import { BiLogoTailwindCss } from 'react-icons/bi';
-import { TbBrandNextjs } from 'react-icons/tb';
-import { BsFillBootstrapFill, BsFiletypeSql } from 'react-icons/bs';
-import { 
-    SiExpress, 
-    SiFastapi, 
-    SiDjango, 
-    SiNodedotjs, 
-    SiMongodb,
-    SiPostgresql,
-    SiTypescript,
-    SiPython,
-    SiJavascript,
-    SiRedux,
-    SiDocker
-} from 'react-icons/si';
+import React, { useTransition, useState } from "react";
+import Image from "next/image";
+import TabButton from "./TabButton";
+import { Element } from "react-scroll";
+import { motion } from "framer-motion";
+import { FaReact } from "react-icons/fa";
+import { BiLogoTailwindCss } from "react-icons/bi";
+import { TbBrandNextjs } from "react-icons/tb";
+import { BsFillBootstrapFill, BsFiletypeSql } from "react-icons/bs";
+import {
+  SiExpress,
+  SiFastapi,
+  SiDjango,
+  SiNodedotjs,
+  SiMongodb,
+  SiPostgresql,
+  SiTypescript,
+  SiPython,
+  SiJavascript,
+  SiRedux,
+  SiDocker,
+  SiMui,
+} from "react-icons/si";
+
+const SkillData = (props) => {
+  const { icon, skillName } = props;
+  return (
+    <li className="flex items-center m-auto">
+      {icon}
+      <span className="flex-shrink-0 ml-2 text-lg">{skillName}</span>
+    </li>
+  );
+};
+
+const frontEndSkills = [
+  {
+    icon: <FaReact />,
+    skillName: "React",
+  },
+  {
+    icon: <SiRedux />,
+    skillName: "Redux & RTK",
+  },
+  {
+    icon: <TbBrandNextjs />,
+    skillName: "Next.js",
+  },
+  {
+    icon: <BiLogoTailwindCss />,
+    skillName: "TailWind",
+  },
+  {
+    icon: <BsFillBootstrapFill />,
+    skillName: "Bootstrap",
+  },
+  {
+    icon: <SiMui />,
+    skillName: "MaterialUi",
+  },
+];
+
+const backEndSkills = [
+  {
+    icon: <SiExpress />,
+    skillName: "Express",
+  },
+  {
+    icon: <SiFastapi />,
+    skillName: "FastAPI",
+  },
+  {
+    icon: <SiDjango />,
+    skillName: "Django",
+  },
+  {
+    icon: <SiNodedotjs />,
+    skillName: "Node.js",
+  },
+  {
+    icon: <SiMongodb />,
+    skillName: "MongoDB",
+  },
+  {
+    icon: <SiPostgresql />,
+    skillName: "PostgresSQL",
+  },
+];
+
+const otherTechnicalSkills = [
+  {
+    icon: <SiTypescript />,
+    skillName: "TypeScript",
+  },
+  {
+    icon: <SiPython />,
+    skillName: "Python",
+  },
+  {
+    icon: <SiJavascript />,
+    skillName: "Javascript ES6",
+  },
+  {
+    icon: <BsFiletypeSql />,
+    skillName: "SQL",
+  },
+  {
+    icon: <SiDocker />,
+    skillName: "Docker",
+  },
+];
 
 const tabData = [
-    {
-        title: "Front-End Skills",
-        id: "FEskills",
-        content: (
-        <ul className="list-disc pl-2 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
-            <li className="flex items-center m-auto">
-                {<FaReact />}
-                <div className="flex-shrink-0 ml-2 text-lg">
-                    React
-                </div>
-            </li>
-            <li className="flex items-center m-auto">
-                {<SiRedux />} 
-                <div className="flex-shrink-0 ml-2 text-lg">
-                    Redux & RTK
-                </div>
-            </li>
-            <li className="flex items-center m-auto">                
-                {<TbBrandNextjs />}
-                <div className="flex-shrink-0 ml-2 text-lg">
-                    Next.js 
-                </div>
-            </li>
-            <li className="flex items-center m-auto">
-                {<BiLogoTailwindCss />}
-                <div className='flex-shrink-0 ml-2 text-lg'>
-                    TailWind
-                </div>
-            </li>
-            <li className="flex items-center m-auto">
-                {<BsFillBootstrapFill />}
-                <div className='flex-shrink-0 ml-2 text-lg'>
-                    Bootstrap
-                </div>
-            </li>
-        </ul>
-        )
-    },
-    {
-        title: "Back-End Skills",
-        id: "BEskills",
-        content: (
-            <ul className="list-disc pl-2 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
-            <li className="flex items-center m-auto">
-                {<SiExpress />}
-                <div className='flex-shrink-0 ml-2 text-lg'>
-                    Express
-                </div>
-            </li>
-            <li className="flex items-center m-auto">
-                {<SiFastapi />}
-                <div className='flex-shrink-0 ml-2 text-lg'>
-                    FastAPI
-                </div>
-            </li>
-            <li className="flex items-center m-auto">
-                {<SiDjango />}
-                <div className='flex-shrink-0 ml-2 text-lg'>
-                    Django 
-                </div>
-            </li>
-            <li className="flex items-center m-auto">
-                Restful APIs
-            </li>
-            <li className="flex items-center m-auto">
-                {<SiNodedotjs />}
-                <div className='flex-shrink-0 ml-2 text-lg'>
-                    Node.js
-                </div> 
-            </li>
-            <li className="flex items-center m-auto">
-                {<SiMongodb />}
-                <div className='flex-shrink-0 ml-2 text-lg'>
-                    MongoDB
-                </div> 
-            </li>
-            <li className="flex items-center m-auto">
-                {<SiPostgresql />}
-                <div className='flex-shrink-0 ml-2 text-lg'>
-                    PostgresSQL 
-                </div>
-            </li>
-            
-        </ul>
-        )
-    },
-    {
-        title: "Other Technical Skills",
-        id: "OSkills",
-        content: (
-            <ul className="list-disc pl-2 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
-            <li className="flex items-center m-auto">
-                {<SiTypescript />}
-                <div className='flex-shrink-0 ml-2 text-lg'>
-                    TypeScript
-                </div> 
-            </li>
-            <li className="flex items-center m-auto">
-                {<SiPython />}
-                <div className='flex-shrink-0 ml-2 text-lg'>
-                    Python
-                </div> 
-            </li>
-            <li className="flex items-center m-auto">
-                {<SiJavascript />}
-                <div className='flex-shrink-0 ml-2 text-lg'>
-                    JavaScript ES6
-                </div> 
-            </li>
-            <li className="flex items-center m-auto">
-                {<BsFiletypeSql />}
-                <div className="flex-shrink-0 ml-2 text-lg">
-                    SQL
-                </div> 
-            </li>
-            <li className="flex items-center m-auto">
-                {<SiDocker />}
-                <div className="flex-shrink-0 ml-2 text-lg">
-                    Docker
-                </div> 
-            </li>
-        </ul>
-        )
-    }   
-
-]
-
+  {
+    title: "Front-End Skills",
+    id: "FEskills",
+    content: (
+      <ul className="list-disc pl-2 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+        {frontEndSkills.map((skill, skillIndex) => {
+          return (
+            <SkillData
+              icon={skill.icon}
+              skillName={skill.skillName}
+              key={skillIndex}
+            />
+          );
+        })}
+        {/* <SkillData icon={<FaReact />} skillName={"React"} /> */}
+        {/* <SkillData icon={<SiRedux />} skillName={"Redux & RTK"} /> */}
+        {/* <SkillData icon={<TbBrandNextjs />} skillName={"Next.js"} /> */}
+        {/* <SkillData icon={<BiLogoTailwindCss />} skillName={"TailWind"} /> */}
+        {/* <SkillData icon={<BsFillBootstrapFill />} skillName={"Bootstrap"} /> */}
+        {/* <SkillData icon={<SiMui />} skillName={"MaterialUi"} /> */}
+        {/* <li className="flex items-center m-auto"> */}
+        {/*   {<FaReact />} */}
+        {/*   <div className="flex-shrink-0 ml-2 text-lg">React</div> */}
+        {/* </li> */}
+        {/* <li className="flex items-center m-auto"> */}
+        {/*   {<SiRedux />} */}
+        {/*   <div className="flex-shrink-0 ml-2 text-lg">Redux & RTK</div> */}
+        {/* </li> */}
+        {/* <li className="flex items-center m-auto"> */}
+        {/*   {<TbBrandNextjs />} */}
+        {/*   <div className="flex-shrink-0 ml-2 text-lg">Next.js</div> */}
+        {/* </li> */}
+        {/* <li className="flex items-center m-auto"> */}
+        {/*   {<BiLogoTailwindCss />} */}
+        {/*   <div className="flex-shrink-0 ml-2 text-lg">TailWind</div> */}
+        {/* </li> */}
+        {/* <li className="flex items-center m-auto"> */}
+        {/*   {<BsFillBootstrapFill />} */}
+        {/*   <div className="flex-shrink-0 ml-2 text-lg">Bootstrap</div> */}
+        {/* </li> */}
+      </ul>
+    ),
+  },
+  {
+    title: "Back-End Skills",
+    id: "BEskills",
+    content: (
+      <ul className="list-disc pl-2 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+        {backEndSkills.map((skill, skillIndex) => {
+          return (
+            <SkillData
+              icon={skill.icon}
+              skillName={skill.skillName}
+              key={skillIndex}
+            />
+          );
+        })}
+        {/* <li className="flex items-center m-auto"> */}
+        {/*   {<SiExpress />} */}
+        {/*   <div className="flex-shrink-0 ml-2 text-lg">Express</div> */}
+        {/* </li> */}
+        {/* <li className="flex items-center m-auto"> */}
+        {/*   {<SiFastapi />} */}
+        {/*   <div className="flex-shrink-0 ml-2 text-lg">FastAPI</div> */}
+        {/* </li> */}
+        {/* <li className="flex items-center m-auto"> */}
+        {/*   {<SiDjango />} */}
+        {/*   <div className="flex-shrink-0 ml-2 text-lg">Django</div> */}
+        {/* </li> */}
+        {/* <li className="flex items-center m-auto">Restful APIs</li> */}
+        {/* <li className="flex items-center m-auto"> */}
+        {/*   {<SiNodedotjs />} */}
+        {/*   <div className="flex-shrink-0 ml-2 text-lg">Node.js</div> */}
+        {/* </li> */}
+        {/* <li className="flex items-center m-auto"> */}
+        {/*   {<SiMongodb />} */}
+        {/*   <div className="flex-shrink-0 ml-2 text-lg">MongoDB</div> */}
+        {/* </li> */}
+        {/* <li className="flex items-center m-auto"> */}
+        {/*   {<SiPostgresql />} */}
+        {/*   <div className="flex-shrink-0 ml-2 text-lg">PostgresSQL</div> */}
+        {/* </li> */}
+      </ul>
+    ),
+  },
+  {
+    title: "Other Technical Skills",
+    id: "OSkills",
+    content: (
+      <ul className="list-disc pl-2 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+        {otherTechnicalSkills.map((skill, skillIndex) => {
+          return (
+            <SkillData
+              icon={skill.icon}
+              skillName={skill.skillName}
+              key={skillIndex}
+            />
+          );
+        })}
+        {/* <li className="flex items-center m-auto"> */}
+        {/*   {<SiTypescript />} */}
+        {/*   <div className="flex-shrink-0 ml-2 text-lg">TypeScript</div> */}
+        {/* </li> */}
+        {/* <li className="flex items-center m-auto"> */}
+        {/*   {<SiPython />} */}
+        {/*   <div className="flex-shrink-0 ml-2 text-lg">Python</div> */}
+        {/* </li> */}
+        {/* <li className="flex items-center m-auto"> */}
+        {/*   {<SiJavascript />} */}
+        {/*   <div className="flex-shrink-0 ml-2 text-lg">JavaScript ES6</div> */}
+        {/* </li> */}
+        {/* <li className="flex items-center m-auto"> */}
+        {/*   {<BsFiletypeSql />} */}
+        {/*   <div className="flex-shrink-0 ml-2 text-lg">SQL</div> */}
+        {/* </li> */}
+        {/* <li className="flex items-center m-auto"> */}
+        {/*   {<SiDocker />} */}
+        {/*   <div className="flex-shrink-0 ml-2 text-lg">Docker</div> */}
+        {/* </li> */}
+      </ul>
+    ),
+  },
+];
 
 const AboutSection = () => {
-    const [tab, setTab] = useState("FEskills");
-    const [isPending, startTransition] = useTransition();
+  const [tab, setTab] = useState("FEskills");
+  const [isPending, startTransition] = useTransition();
 
-    const handleTabChange = (id) => {
-        startTransition(() => {
-            setTab(id);
-        });
-    }
+  const handleTabChange = (id) => {
+    startTransition(() => {
+      setTab(id);
+    });
+  };
 
-    const containerVariants = {
-        hidden: { opacity: 0 },
-        visible: {
-            opacity: 1,
-            transition: {
-                when: "beforeChildren",
-                staggerChildren: 0.2,
-            },
-        },
-    };
-    
-    const childVariants = {
-        hidden: { opacity: 0, y: 20 }, 
-        visible: { opacity: 1, y: 0 },
-    };
+  const containerVariants = {
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 1,
+      transition: {
+        when: "beforeChildren",
+        staggerChildren: 0.2,
+      },
+    },
+  };
 
-    return (
-        <Element name="about">
-            <section className='text-white'>
-                <motion.div 
-                    variants={containerVariants}
-                    initial="hidden"
-                    animate="visible"
-                    className='md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16'
-                >
-                    <Image 
-                        src='./images/codingComputer.jpeg' 
-                        alt='About Section' 
-                        width={500} 
-                        height={500}
-                        sizing="intrinsic" 
-                    />
-                    <motion.div 
-                        className='mt-4 md:mt-0 text-left flex flex-col h-full'
-                        variants={childVariants}
-                        >
-                        <motion.h2 
-                            className='text-4xl font-bold text-white mb-4'
-                            variants={childVariants}
-                        >
-                            About Me
-                        </motion.h2>
-                        <motion.p 
-                            className='text-base md:text-lg'
-                            variants={childVariants}
-                        >
-                            I&apos;m a Full Stack Developer from San Francisco Bay Area, California. I&apos;ve transitioned from tech operations and jumped headfirst into the exhilarating world of software development. 
-                            I&apos;m all about crafting digital solutions and making things happen in the tech universe. Let&apos;s code some awesome stuff! 🚀
-                        </motion.p>
-                        <div className='flex flex-row justify-start mt-8'>
-                            <TabButton 
-                            selectTab={() => handleTabChange("FEskills")}
-                            active={tab === "FEskills"}
-                            >
-                                {" "}
-                                Front-End Skills {" "}
-                            </TabButton>
-                            <TabButton 
-                            selectTab={() => handleTabChange("BEskills")}
-                            active={tab === "BEskills"} 
-                            >
-                                {" "}
-                                Back-End Skills {" "}
-                            </TabButton>
-                            <TabButton 
-                            selectTab={() => handleTabChange("OSkills")}
-                            active={tab === "OSkills"} 
-                            >
-                                {" "}
-                                Other Technical Skills {" "}
-                            </TabButton>
-                        </div>
-                        <div className='mt-8'>
-                            <div className='flex flex-col gap-20'>
-                                {tabData.find((t) => t.id === tab).content}
-                            </div>
-                        </div>
-                    </motion.div>
-                </motion.div>
-            </section>
-        </Element>
-    )
-}
+  const childVariants = {
+    hidden: { opacity: 0, y: 20 },
+    visible: { opacity: 1, y: 0 },
+  };
+
+  return (
+    <Element name="about">
+      <section className="text-white">
+        <motion.div
+          variants={containerVariants}
+          initial="hidden"
+          animate="visible"
+          className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16"
+        >
+          <Image
+            src="./images/codingComputer.jpeg"
+            alt="About Section"
+            width={500}
+            height={500}
+            sizing="intrinsic"
+          />
+          <motion.div
+            className="mt-4 md:mt-0 text-left flex flex-col h-full"
+            variants={childVariants}
+          >
+            <motion.h2
+              className="text-4xl font-bold text-white mb-4"
+              variants={childVariants}
+            >
+              About Me
+            </motion.h2>
+            <motion.p className="text-base md:text-lg" variants={childVariants}>
+              I&apos;m a Front-End Developer from San Francisco Bay Area,
+              California. I&apos;ve transitioned from tech operations and jumped
+              headfirst into the exciting world of software development. 🚀
+            </motion.p>
+            <div className="flex flex-row justify-start mt-8">
+              <TabButton
+                selectTab={() => handleTabChange("FEskills")}
+                active={tab === "FEskills"}
+              >
+                {" "}
+                Front-End Skills{" "}
+              </TabButton>
+              <TabButton
+                selectTab={() => handleTabChange("BEskills")}
+                active={tab === "BEskills"}
+              >
+                {" "}
+                Back-End Skills{" "}
+              </TabButton>
+              <TabButton
+                selectTab={() => handleTabChange("OSkills")}
+                active={tab === "OSkills"}
+              >
+                {" "}
+                Other Technical Skills{" "}
+              </TabButton>
+            </div>
+            <div className="mt-8">
+              <div className="flex flex-col gap-20">
+                {tabData.find((t) => t.id === tab).content}
+              </div>
+            </div>
+          </motion.div>
+        </motion.div>
+      </section>
+    </Element>
+  );
+};
 
 export default AboutSection;
+
